07ece721a9de66ae5039a0ab7ac4c2de496466f2
Mustafa Altuntas <93078239+altuntas-mustafa@users.noreply.github.com>
2024-05-23 14:26:36 +0200

    server changed

--- backend/server.js
+++ backend/server.js
@@ -20,46 +20,46 @@ const PORT = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Read PDF and parse words
-const pdfPath = './e-book-lesson-1-20-1000DutchWords.pdf';
+// // Read PDF and parse words
+// const pdfPath = './e-book-lesson-1-20-1000DutchWords.pdf';
 
-async function parseAndSaveWords() {
-  const dataBuffer = fs.readFileSync(pdfPath);
-  const data = await pdf(dataBuffer);
-  const words = extractWords(data.text);
-  await saveWordsToFirestore(words);
-}
+// async function parseAndSaveWords() {
+//   const dataBuffer = fs.readFileSync(pdfPath);
+//   const data = await pdf(dataBuffer);
+//   const words = extractWords(data.text);
+//   await saveWordsToFirestore(words);
+// }
 
-function extractWords(text) {
-  const lines = text.split('\n');
-  const words = [];
-  let lesson = '';
+// function extractWords(text) {
+//   const lines = text.split('\n');
+//   const words = [];
+//   let lesson = '';
 
-  lines.forEach(line => {
-    if (line.startsWith('LESSON')) {
-      lesson = line;
-    } else {
-      const parts = line.split(' ');
-      if (parts.length > 1) {
-        const dutch = parts[0];
-        const english = parts.slice(1).join(' ');
-        words.push({ dutch, english, lesson });
-      }
-    }
-  });
+//   lines.forEach(line => {
+//     if (line.startsWith('LESSON')) {
+//       lesson = line;
+//     } else {
+//       const parts = line.split(' ');
+//       if (parts.length > 1) {
+//         const dutch = parts[0];
+//         const english = parts.slice(1).join(' ');
+//         words.push({ dutch, english, lesson });
+//       }
+//     }
+//   });
 
-  return words;
-}
+//   return words;
+// }
 
-async function saveWordsToFirestore(words) {
-  const batch = db.batch();
-  words.forEach((word, index) => {
-    const docRef = db.collection('words').doc(`word${index + 1}`);
-    batch.set(docRef, word);
-  });
-  await batch.commit();
-  console.log('Words have been saved to Firestore');
-}
+// async function saveWordsToFirestore(words) {
+//   const batch = db.batch();
+//   words.forEach((word, index) => {
+//     const docRef = db.collection('words').doc(`word${index + 1}`);
+//     batch.set(docRef, word);
+//   });
+//   await batch.commit();
+//   console.log('Words have been saved to Firestore');
+// }
 
 app.get('/api/words', async (req, res) => {
   try {
@@ -83,5 +83,5 @@ app.post('/api/words/known', async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-  parseAndSaveWords();
+//   parseAndSaveWords();
 });
